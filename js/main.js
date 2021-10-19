ba9c114674adfd7ca7fc6ebbeb4033941d8de4b0
Anfelealo <anfelealo@gmail.com>
2021-10-18 21:59:47 -0500

    Advanced on js script. Still pending reviews

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-var movies = [
+var movies =  [
     {
-        rating: 2,
-        name: "ElTitanic",
+        rating: document.getElementById("mf-rating"),
+        name: document.getElementById("mf-name"),
         image: "https://m.media-amazon.com/images/I/51mTtUGvUCL.jpg"
-    },
-    {
-        rating: 3,
-        name: "Interstellar",
-        image: "https://pics.filmaffinity.com/Interstellar-366875261-large.jpg"
     }
-]
+];
 
-function addMovies(){
+function addMovies(){    
     for(let i = 0;  i < movies.length; i++){
-        let cardList = document.getElementById("ml-row-styling");
+        let cardList = document.getElementById("cards-list");
         let card = '<div class="col-sm ml-col-styling">'+'<div class="card bg-transparent ml-card">'+
             '<img src="'+movies[i].image+'" alt="'+movies[i].name+'" class="movie-card-image">'+
-            '<i class="fas fa-star starUp'+movies[i].name+'"></i>'+'<i class="fas fa-star starCent'
-            +movies[i].name+'"></i>'+'<i class="fas fa-star starLow'+movies[i].name+'"></i></div></div></div>';
+            '<div class="ml-card-img-overlay">'+
+            '<i class="fas fa-star starUp'+movies[i].name+' stars"></i>'+'<i class="fas fa-star starCent'
+            +movies[i].name+' stars"></i>'+'<i class="fas fa-star starLow'+movies[i].name+
+            ' stars"></i></div></div></div>';
         cardList.innerHTML += card
     }
 }
 
-addMovies();
+
